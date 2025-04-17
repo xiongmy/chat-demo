c638d8cbef731166a067a8c1c3ba8fafe1ce5abe
amy <aimee_y12@163.com>
2025-04-17 17:02:16 +0800

    fix: add electron

---
+++ chat-web/src/main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL('http://10.168.1.127:5173/');
+}
+
+app.whenReady().then(createWindow);
